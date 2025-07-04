5124b3b305ed3642c5d5ebda17de94abe0d9208a
MUHAMMED JUBAL AHMED <freelancerjubal@gmail.com>
2025-07-05 01:55:58 +0600

    Create i18n.js

---
+++ src/i18n.js
@@ -0,0 +1,10 @@
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
+import en from '../locales/en.json';
+import bn from '../locales/bn.json';
+
+i18n.translations = { en, bn };
+i18n.locale = Localization.locale.startsWith('bn') ? 'bn' : 'en';
+i18n.fallbacks = true;
+
+export default i18n;
